8bb1621bed208c8ca3d8719cc9d53670cc06a78d
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-02 09:51:18 -0800

    rebuild navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+import logo from '../img/logo.png';
 
 export const NavBar =()=> {
   return(
-    <Navbar bg="dark"className="pb-2 " variant="dark">
-    <Navbar.Brand href="#home">
-      React Bootstrap
-    </Navbar.Brand>
-  </Navbar>
+   <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
+     <div className="container-fluid">
+       <img src={logo} alt="" />
+       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+         <span className="navbar-toggler-icon"></span>
+       </button>
+       <div className="collapse navbar-collapse" id="navbarResponsive">
+         <ul className="navbar-nav ml-auto">
+           <li className="nav-item">
+             <a className="nav-link active" href="#">Home</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">About</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Services</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Team</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Contact</a>
+           </li>
+         </ul>
+       </div>
+     </div>
+   </nav>
   )
 }
 
